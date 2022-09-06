f4e97d3fc358b5239d3ffded113937a2a8fb056e
sni <>
2022-09-07 04:27:14 +0900

    setting basic DOM

--- app/src/page/index.tsx
+++ app/src/page/index.tsx
@@ -1,5 +1,43 @@
 import * as React from 'react';
+import { category } from '../data/category';
 
-export default () => {
-  return <div>hoge</div>;
+const Page = () => {
+  const [selectedCategory, setSelectedCategory] = React.useState("");
+
+  const createCategoryProductsDOM = () => {
+    if (!selectedCategory) {
+      return <p>カテゴリが選択されていません</p>
+    }
+
+    return <p>{selectedCategory}</p>
+  };
+
+  return (
+    <div>
+      <main>
+        <section>
+          <ul>
+            {Object.values(category).map(v => {
+              return (
+                <li key={v}>
+                  <button
+                    type="button"
+                    onClick={() => {setSelectedCategory(v)}}
+                  >
+                    {v}
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+
+        <section>
+          {createCategoryProductsDOM()}
+        </section>
+      </main>
+    </div>
+  );
 };
+
+export default Page;
