7769d769c1f8b2d0f63437f0940502dd01b55212
sni <>
2022-09-07 03:12:04 +0900

    separate category and individual date

--- app/src/data/category.ts
+++ app/src/data/category.ts
@@ -1,5 +1,3 @@
-import { schedule } from "./schedule";
-
 export const category = {
   food: "保存食",
   armor: "防具",
@@ -17,14 +15,4 @@ export const category = {
   animal: "動物製品",
   marine: "海産製品",
   ceramic: "窯業製品"
-};
-
-export const food = {
-  0: {
-    id: 0,
-    name: "開拓工房のザワークラウト",
-    schedule: schedule.four,
-    amount: 48,
-    type: category.food
-  }
 };
