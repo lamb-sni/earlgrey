60ade9bf68ef8b049f5f2e396123efb0472be543
sni <sni@sni-2.local>
2022-09-07 01:45:18 +0900

    create react app

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Page from "./page";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Page />
+    </>
+  );
+}
+
+export default App;
---
+++ app/src/page/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+
+export default () => {
+  return <div>hoge</div>;
+};
