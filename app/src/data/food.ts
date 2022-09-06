4f54373f780649bbd7805e49d7d5fa4098275eb5
sni <>
2022-09-07 03:18:25 +0900

    delete id

--- app/src/data/food.ts
+++ app/src/data/food.ts
@@ -3,38 +3,31 @@ import { category } from "./category";
 
 export const food = {
   0: {
-    id: 0,
     name: "開拓工房のザワークラウト",
     schedule: schedule.four,
     amount: 48,
     type: [category.food],
+    bonus: [],
   },
   1: {
-    id: 1,
     name: "開拓工房のコーンフレーク",
     schedule: schedule.four,
     amount: 62,
-    type: [category.food]
+    type: [category.food],
+    bonus: [],
   },
   2: {
-    id: 2,
     name: "アイルソルコット",
     schedule: schedule.six,
     amount: 64,
-    type: [category.food, category.marine]
+    type: [category.food, category.marine],
+    bonus: [],
   },
   3: {
-    id: 3,
     name: "開拓工房のラディッシュピクルス	",
     schedule: schedule.eight,
     amount: 124,
-    type: [category.food]
+    type: [category.food],
+    bonus: [],
   },
-};
-
-export const bonus = {
-  0: [food[1], food[2], food[3]],
-  1: [food[0], food[2], food[3]],
-  2: [food[0], food[1], food[3]],
-  3: [food[0], food[1], food[2]],
 };
--- app/src/data/schedule.ts
+++ app/src/data/schedule.ts
@@ -1,16 +1,13 @@
 export const schedule = {
   four: {
-    id: 0,
     value: 4,
     label: "4時間"
   },
   six: {
-    id: 1,
     value: 6,
     label: "4時間"
   },
   eight: {
-    id: 2,
     value: 8,
     label: "8時間"
   }
@@ -18,31 +15,24 @@ export const schedule = {
 
 export const schedulePattern = {
   0: {
-    id: 0,
     label: "4時間 4時間 4時間 4時間 4時間 4時間"
   },
   1: {
-    id: 1,
     label: "4時間 4時間 4時間 4時間 8時間"
   },
   2: {
-    id: 2,
     label: "4時間 4時間 4時間 6時間 6時間"
   },
   3: {
-    id: 3,
     label: "6時間 6時間 6時間 6時間"
   },
   4: {
-    id: 4,
     label: "4時間 6時間 6時間 8時間"
   },
   5: {
-    id: 5,
     label: "4時間 4時間 8時間 8時間"
   },
   6: {
-    id: 6,
     label: "8時間 8時間 8時間"
   }
 };
