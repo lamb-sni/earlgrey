4d8f187b3077c5b9ab0a93f2475f989150a494d5
sni <>
2022-09-07 03:30:28 +0900

    delete bonus key

--- app/src/data/food.ts
+++ app/src/data/food.ts
@@ -7,27 +7,23 @@ export const food = {
     schedule: schedule.four,
     amount: 48,
     category: [category.food],
-    bonus: [],
   },
   1: {
     name: "開拓工房のコーンフレーク",
     schedule: schedule.four,
     amount: 62,
     category: [category.food],
-    bonus: [],
   },
   2: {
     name: "アイルソルコット",
     schedule: schedule.six,
     amount: 64,
     category: [category.food, category.marine],
-    bonus: [],
   },
   3: {
     name: "開拓工房のラディッシュピクルス	",
     schedule: schedule.eight,
     amount: 124,
     category: [category.food],
-    bonus: [],
   },
 };
--- app/src/data/snack.ts
+++ app/src/data/snack.ts
@@ -7,27 +7,23 @@ export const snack = {
     schedule: schedule.six,
     amount: 86,
     type: [category.snack],
-    bonus: [],
   },
   1: {
     name: "開拓工房のフィッシュパイ",
     schedule: schedule.six,
     amount: 93,
     type: [category.snack, category.marine],
-    bonus: [],
   },
   2: {
     name: "開拓工房のパンプキンプディング",
     schedule: schedule.six,
     amount: 93,
     type: [category.snack],
-    bonus: [],
   },
   3: {
     name: "開拓工房のキャラメル",
     schedule: schedule.six,
     amount: 97,
     type: [category.snack],
-    bonus: [],
   },
 };
