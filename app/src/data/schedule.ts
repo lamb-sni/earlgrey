af7b2c92a7e6d6d9608618b051db0325be09d5c4
sni <>
2022-09-07 02:56:04 +0900

    change array -> obj

--- app/src/data/schedule.ts
+++ app/src/data/schedule.ts
@@ -1,48 +1,48 @@
-export const schedule = [
-  {
+export const schedule = {
+  0: {
     id: 0,
     value: 4,
     label: "4時間"
   },
-  {
+  1: {
     id: 1,
     value: 6,
     label: "4時間"
   },
-  {
+  2: {
     id: 2,
     value: 8,
     label: "8時間"
   }
-];
+};
 
-export const schedulePattern = [
-  {
+export const schedulePattern = {
+  0: {
     id: 0,
     label: "4時間 4時間 4時間 4時間 4時間 4時間"
   },
-  {
+  1: {
     id: 1,
     label: "4時間 4時間 4時間 4時間 8時間"
   },
-  {
+  2: {
     id: 2,
     label: "4時間 4時間 4時間 6時間 6時間"
   },
-  {
+  3: {
     id: 3,
     label: "6時間 6時間 6時間 6時間"
   },
-  {
+  4: {
     id: 4,
     label: "4時間 6時間 6時間 8時間"
   },
-  {
+  5: {
     id: 5,
     label: "4時間 4時間 8時間 8時間"
   },
-  {
+  6: {
     id: 6,
     label: "8時間 8時間 8時間"
   }
-]
+};
