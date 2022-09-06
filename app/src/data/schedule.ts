8029da80be691c4755a6fecdc70bd4cb97d2a2f1
sni <>
2022-09-07 02:41:03 +0900

    add schedule data

---
+++ app/src/data/schedule.ts
@@ -0,0 +1,48 @@
+export const schedule = [
+  {
+    id: 0,
+    value: 4,
+    label: "4時間"
+  },
+  {
+    id: 1,
+    value: 6,
+    label: "4時間"
+  },
+  {
+    id: 2,
+    value: 8,
+    label: "8時間"
+  }
+];
+
+export const schedulePattern = [
+  {
+    id: 0,
+    label: "4時間 4時間 4時間 4時間 4時間 4時間"
+  },
+  {
+    id: 1,
+    label: "4時間 4時間 4時間 4時間 8時間"
+  },
+  {
+    id: 2,
+    label: "4時間 4時間 4時間 6時間 6時間"
+  },
+  {
+    id: 3,
+    label: "6時間 6時間 6時間 6時間"
+  },
+  {
+    id: 4,
+    label: "4時間 6時間 6時間 8時間"
+  },
+  {
+    id: 5,
+    label: "4時間 4時間 8時間 8時間"
+  },
+  {
+    id: 6,
+    label: "8時間 8時間 8時間"
+  }
+]
