17efab00aafb153bf499172e799bc1ce91c6c219
sni <>
2022-09-15 04:40:59 +0900

    vhを動的に取得

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,6 +4,10 @@ import Page from "./page";
 import "./reset.css";
 
 function App() {
+  React.useEffect(() => {
+    document.documentElement.style.setProperty("--vh", `${window.innerHeight * 0.01}px`);
+  }, []);
+
   return (
     <RecoilRoot>
       <Page />
